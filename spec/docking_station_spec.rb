e7485c3c01a050eb6f93f24cffca79c9380e4efa
Heli Sivunen <heli@mooncat.tv>
2019-09-17 16:15:08 +0100

    unit test DockingStation responds to release_bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,10 @@
 require 'docking_station'
 
 describe DockingStation do
+  it "responds to release_bike" do
+    instance = DockingStation.new
+    expect(instance).to respond_to(:release_bike)
+  end
+
+  # it { is_expected.to respond_to release_bike }
 end
