e2e0571193d922d29f9f6f85b57942484874afea
Heli Sivunen <heli@mooncat.tv>
2019-09-18 16:56:32 +0100

    Add code for finding docking station with bike and only releasing it if there is one.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,8 +1,16 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader :bike
+
   def release_bike
-    @bike = Bike.new
+    @bike
     #needs to give us a bike object
   end
+
+  def dock(bike)
+   #add bike to self
+   @bike = bike
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,10 +7,28 @@ describe DockingStation do
   end
   # it { is_expected.to respond_to release_bike }
 
-
-  it "releases working bikes" do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  describe '#release_bike' do
+    it "releases bike" do
+      bike = Bike.new
+      subject.dock(bike)
+      #release docked bike
+      expect(subject.release_bike).to eq bike
+    end
   end
 
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it { is_expected.to respond_to(:bike) }
+  # it "responds to bike" do
+  #   instance = DockingStation.new
+  #   expect(instance).to respond_to(:bike)
+  it "returns docked bikes" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
+  it "should return an error if empty dock" do
+
+  end
 end
