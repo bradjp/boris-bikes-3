f9043a50c51c4d831c4579951af702d42a7c879f
Brad Phillips <bjrp@protonmail.com>
2019-09-19 16:36:18 +0100

    Set a default capacity to docking station, added private methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,21 +1,40 @@
 require_relative 'bike'
 
 class DockingStation
-  attr_reader :bike
+  attr_reader :bikes, :capacity
+
+  DEFAULT_CAPACITY = 20
+
+  def initialize(capacity=DEFAULT_CAPACITY)
+  
+  @bikes = []
+  @capacity = capacity
+  end
+
+  
 
   def release_bike
     #guard condition
-    raise 'No bikes available' unless @bike
+    raise 'No bikes available' if empty?
     #return bike if bike available
-    @bike
+    @bikes.pop
   
   end
 
   def dock(bike)
     #guard condition
-    fail "Docking station full" if @bike
+    fail "Docking station full" if full?
    #add bike to docking station object
-   @bike = bike
+   @bikes.push(bike)  
+  end
+  
+  private
+  def full?
+    return true if @bikes.length >= DEFAULT_CAPACITY
+
   end
 
+  def empty?
+    return true if @bikes.empty?
+  end 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,7 +5,14 @@ describe DockingStation do
     instance = DockingStation.new
     expect(instance).to respond_to(:release_bike)
   end
-  # it { is_expected.to respond_to release_bike }
+  it "creates a docking station object with a default value 20" do
+    docking_station = DockingStation.new
+    expect(docking_station.capacity).to eq 20
+  end
+  it "takes an argument specifying number of bikes" do
+  docking_station = DockingStation.new(40)
+  expect(docking_station.capacity).to eq 40
+  end
 
   describe '#release_bike' do
     it "releases bike" do
@@ -18,12 +25,12 @@ describe DockingStation do
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
-  it { is_expected.to respond_to(:bike) }
+  it { is_expected.to respond_to(:bikes) }
   
   it "returns docked bikes" do
     bike = Bike.new
     subject.dock(bike)
-    expect(subject.bike).to eq bike
+    expect(subject.bikes).to include bike
   end
   describe '#release_bike'
   it "should raise an error when dock is empty" do
@@ -31,11 +38,12 @@ describe DockingStation do
   end
   describe '#dock' do
   it 'raises an error when dock is full' do
-    subject.dock(Bike.new)
+    subject.capacity.times { subject.dock(Bike.new) }
     expect { subject.dock(Bike.new) }.to raise_error 'Docking station full'
     end
   end
 
+  
   
 
 end
